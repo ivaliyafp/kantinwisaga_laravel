0c8b7f8d9a9b79fa5bf261783d89e65280a72e46
ivaliyafp <ivaliyafp181006@gmail.com>
2024-05-15 14:49:05 +0700

    mengubah foto jadi kecil

--- resources/views/items/index.blade.php
+++ resources/views/items/index.blade.php
@@ -33,18 +33,37 @@
     <div class="welcome-container">
         <img src="{{ asset('imgs/kantin.jpeg') }}"  alt="kantin.jpeg" class="profile-image">
     </div>
-    <div class="menulist">
-        <div class="makanan">
-            <img src="{{ asset('imgs/yummy.jpeg') }}" alt="makanan_1">
+    <div class="container">
+    <div class="row">
+        <div class="col-md-2">
+            <div class="card mb-2">
+                <img class="card-img-top" src="{{ asset('imgs/bakso (1).jpeg') }}" alt="Gambar Bakso" style="width: 100px; height: 100px;">
+                <div class="card-body">
+                    <h5 class="card-title">Makanan Bakso</h5>
+                    <p class="card-text">Ini adalah deskripsi singkat mengenai makanan bakso.</p>
+                </div>
+            </div>
         </div>
-        <div class="makanan">
-            <img src="{{ asset('imgs/bakso(1).jpeg') }}" alt="makanan_2">
+        <div class="col-md-4">
+            <div class="card mb-4">
+                <img class="card-img-top" src="{{ asset('imgs/makanan (1).jpeg') }}" alt="Makanan 3" style="width: 100px; height: 100px;">
+                <div class="card-body">
+                    <h5 class="card-title">Judul Makanan 1</h5>
+                    <p class="card-text">Deskripsi singkat mengenai makanan 1.</p>
+                </div>
+            </div>
         </div>
-        <div class="makanan">
-            <img src="{{ asset('imgs/makanan(1).jpeg') }}" alt="makanan_3">
+        <div class="col-md-4">
+            <div class="card mb-4">
+                <img class="card-img-top" src="{{ asset('imgs/makanan (2).jpeg') }}" alt="Makanan 4" style="width: 100px; height: 100px;">
+                <div class="card-body">
+                    <h5 class="card-title">Judul Makanan 2</h5>
+                    <p class="card-text">Deskripsi singkat mengenai makanan 2.</p>
+                </div>
+            </div>
         </div>
-        <div class="makanan">
-            <img src="{{ asset('imgs/makanan(2).jpeg') }}" alt="makanan_4">
     </div>
+</div>
+
 </body>
 </html>
